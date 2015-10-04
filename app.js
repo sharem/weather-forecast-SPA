3655efda4dbbc299c4d41eebaf09bef68810d99f
Iraide Diaz Orueta <iraide.diaz@gmail.com>
2015-10-04 17:48:03 +0200

    OpenWeatherMap.org API linked + started working on the output

--- app.js
+++ app.js
@@ -14,14 +14,46 @@ weatherApp.config(function ($routeProvider) {
             templateUrl: 'pages/forecast.htm',
             controller: 'forecastController'  
         })
+    
+        .when('/forecast/:days', {
+            templateUrl: 'pages/forecast.htm',
+            controller: 'forecastController'  
+        })
+    
 
 });
 
-// Controllers
-weatherApp.controller('homeController', ['$scope', function($scope) {
+// Service
 
+weatherApp.service('cityService', function() {
+    
+    this.city = "New York, NY";
+    
+});
+
+// Controllers
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function(){
+        cityService.city = $scope.city;
+    });
+    
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+    
+    $scope.city = cityService.city;
+    $scope.days = $routeParams.days || 1;
+    
+    // E.j.: http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=7
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {callback: "JSON_CALLBACK"}, { get: { method: "JSONP"}});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, units: "metric", cnt:$scope.days });
+    
+    $scope.convertToDate = function(dt) {
+        return new Date(dt * 1000);
+    }
 
 }]);
