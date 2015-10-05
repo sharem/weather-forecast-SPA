e374161f5525e9005c159fb358b3f03d22741651
Iraide Diaz Orueta <iraide.diaz@gmail.com>
2015-10-05 16:47:48 +0200

    Changed form submit

--- controllers.js
+++ controllers.js
@@ -1,11 +1,15 @@
 // Controllers
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
     
     $scope.city = cityService.city;
     
     $scope.$watch('city', function(){
         cityService.city = $scope.city;
     });
+    
+    $scope.submit = function() {
+        $location.path("/forecast");
+    }
     
 }]);
 
